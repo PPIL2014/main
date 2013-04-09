b6f8bfcd73e6ca2971fee79b210d6046ed0c5b04
yannH_G12 <yann.hortance7@etu.univ-lorraine.fr>
2013-04-09 13:07:26 +0200

    changement des href pour redirections par les faces/

---
+++ EasyJob/EasyJob-ejb/src/java/business/UtilisateurEJB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Yann
+ */
+@Stateless
+@LocalBean
+public class UtilisateurEJB {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
