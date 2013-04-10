852fe9c4007b44fa28e3015d0bea6ae18f811a13
Enis Kulla <enis.kulla6@etu.univ-lorraine.fr>
2013-04-10 14:57:21 +0200

    ProfilEmpManagedBean.java modifié

--- EasyJob/EasyJob-war/src/java/ManagedBean/ProfilEmpManagedBean.java
+++ EasyJob/EasyJob-war/src/java/ManagedBean/ProfilEmpManagedBean.java
@@ -4,9 +4,17 @@
  */
 package ManagedBean;
 
+import interfaces.EntrepriseLocal;
 import java.io.Serializable;
+import java.util.GregorianCalendar;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+import persistence.Candidat;
+import persistence.Employeur;
+import persistence.Entreprise;
 
 /**
  *
@@ -17,144 +25,32 @@ import javax.inject.Named;
 @SessionScoped
 public class ProfilEmpManagedBean implements Serializable{
     
-    private String nomEntr="aaaa";
-    private String domaineActi="zaeaze";
-    private String rue;
-    private String codePostal;
-    private String ville;
-    private String pays;
-    private String telephoneEnt;
-    private String statut;
-    private String siteWeb;
-    private String description;
-    private int nbEmployes;
-    private String mail;
-    private String nom;
-    private String prenom;
-    private String telephoneEmp;
+    private Employeur employeur;
+    private Entreprise entreprise;
     
+    @Inject
+    EntrepriseLocal entrepriseEJB;
     
     public ProfilEmpManagedBean(){
         
     }
-
-    public String getNomEntr() {
-        return nomEntr;
+    
+    @PostConstruct
+    public void initialisation() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        RegisterManageBean rmb = (RegisterManageBean) fc.getExternalContext().getSessionMap().get("connexionBean");
+        System.out.println(rmb+"---------------------------------");
+        System.out.println(rmb.getCandidat());
+        GregorianCalendar date = new GregorianCalendar(1978, 0, 7);
+        employeur = new Employeur("test","test","0395864899","1526975694102",true,entreprise);
+        System.out.println("+++++++++++++++++++++");
     }
 
-    public void setNomEntr(String nomEntr) {
-        this.nomEntr = nomEntr;
+    public Employeur getEmployeur() {
+        return employeur;
     }
 
-    public String getDomaineActi() {
-        return domaineActi;
-    }
-
-    public void setDomaineActi(String domaineActi) {
-        this.domaineActi = domaineActi;
-    }
-
-    public String getRue() {
-        return rue;
-    }
-
-    public void setRue(String rue) {
-        this.rue = rue;
-    }
-
-    public String getCodePostal() {
-        return codePostal;
-    }
-
-    public void setCodePostal(String codePostal) {
-        this.codePostal = codePostal;
-    }
-
-    public String getVille() {
-        return ville;
-    }
-
-    public void setVille(String ville) {
-        this.ville = ville;
-    }
-
-    public String getPays() {
-        return pays;
-    }
-
-    public void setPays(String pays) {
-        this.pays = pays;
-    }
-
-    public String getTelephoneEnt() {
-        return telephoneEnt;
-    }
-
-    public void setTelephoneEnt(String telephoneEnt) {
-        this.telephoneEnt = telephoneEnt;
-    }
-
-    public String getStatut() {
-        return statut;
-    }
-
-    public void setStatut(String statut) {
-        this.statut = statut;
-    }
-
-    public String getSiteWeb() {
-        return siteWeb;
-    }
-
-    public void setSiteWeb(String siteWeb) {
-        this.siteWeb = siteWeb;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getNbEmployes() {
-        return nbEmployes;
-    }
-
-    public void setNbEmployes(int nbEmployes) {
-        this.nbEmployes = nbEmployes;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getTelephoneEmp() {
-        return telephoneEmp;
-    }
-
-    public void setTelephoneEmp(String telephoneEmp) {
-        this.telephoneEmp = telephoneEmp;
+    public void setEmployeur(Employeur employeur) {
+        this.employeur = employeur;
     }
 }
