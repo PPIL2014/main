42dd3dcccd038ec69c4b71a6768fd2f3c7225faa
FERGOUG Anass <anass.fergoug@gmail.com>
2013-04-12 12:13:37 +0200

    ProfilEmp fixed

--- EasyJob/EasyJob-war/src/java/ManagedBean/ProfilEmpManagedBean.java
+++ EasyJob/EasyJob-war/src/java/ManagedBean/ProfilEmpManagedBean.java
@@ -5,31 +5,24 @@
 package ManagedBean;
 
 import interfaces.EmployeurLocal;
-import interfaces.EntrepriseLocal;
 import java.io.Serializable;
-import java.util.GregorianCalendar;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
-import javax.inject.Named;
-import persistence.Adresse;
 import persistence.Employeur;
-import persistence.Entreprise;
 
 /**
  *
  * @author enis
  */
 
-@Named
-@SessionScoped
+@ManagedBean
 public class ProfilEmpManagedBean implements Serializable{
     
     private Employeur employeur;
     
     @Inject
-    EmployeurLocal employeurLocal;
+    EmployeurLocal employeurEJB;
     
     public ProfilEmpManagedBean(){
         
@@ -37,14 +30,15 @@ public class ProfilEmpManagedBean implements Serializable{
     
     @PostConstruct
     public void initialisation() {
-        FacesContext fc = FacesContext.getCurrentInstance();
+        /*FacesContext fc = FacesContext.getCurrentInstance();
         RegisterManageBean rmb = (RegisterManageBean) fc.getExternalContext().getSessionMap().get("connexionBean");
         System.out.println(rmb+"---------------------------------");
         System.out.println(rmb.getCandidat());
         GregorianCalendar date = new GregorianCalendar(1978, 0, 7);
         Entreprise entreprise = new Entreprise("Societe Generale","banque","0155687582","SARL", new Adresse("35 allée des Lilas","75000","Paris","France"));
         employeur = new Employeur("test","test","0395864899","1526975694102",true,entreprise);
-        System.out.println("+++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++");*/
+        employeur = employeurEJB.getEmployeurByMail("laure@test.fr");
     }
 
     public Employeur getEmployeur() {
