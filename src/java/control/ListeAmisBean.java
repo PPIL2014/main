065c61e3abdac117e5ff09431472e2c12db398ee
thomas <thomas@thomas-PC>
2014-04-24 00:55:42 +0200

    Mise au propre du projet (respect modèle) Ajout fonctionnalité recherche d'amis (dans la page profil.xhtml)

--- src/java/control/ListeAmisBean.java
+++ src/java/control/ListeAmisBean.java
@@ -2,13 +2,17 @@ package control;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.transaction.UserTransaction;
 import model.*;
@@ -23,12 +27,26 @@ public class ListeAmisBean {
     @Resource 
     private UserTransaction ut;
     
+    private String pseudoAmi;
+    
     public ListeAmisBean() {
         
+    }
+
+    public String getPseudoAmi() {
+        return pseudoAmi;
+    }
+
+    public void setPseudoAmi(String pseudoAmi) {
+        this.pseudoAmi = pseudoAmi;
     }
     
     public String afficherAmis () {
         return "listeAmis.xhtml" ;
+    }
+    
+    public String rechercherAmis() {
+        return "rechercherAmis.xhtml";
     }
     
     public Collection<Contact> getContacts () {
@@ -37,6 +55,16 @@ public class ListeAmisBean {
     
     public boolean estConnecte (Utilisateur u) {
         return u.getSession().getEstConnecte();
+    }
+    
+    public Collection<Utilisateur> rechercherListeAmis() {
+        System.out.println(pseudoAmi);
+        if(pseudoAmi == null)
+            return new ArrayList<>();
+
+        Query query = em.createQuery("SELECT u FROM Utilisateur u where u.pseudo LIKE :pseudo");
+        query.setParameter("pseudo", "%"+pseudoAmi+"%");
+        return (List<Utilisateur>) query.getResultList();
     }
     
     public Utilisateur getUtilisateurSession () {
