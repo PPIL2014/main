b6262927cd92af3aec0c2479569218c85cd4be1f
Geoffrey <Geoffrey@10.10.119.130>
2014-04-24 13:03:17 +0200

    Ajout fonction ajouter/bloquer contact à la fin du chat.

--- src/java/model/Utilisateur.java
+++ src/java/model/Utilisateur.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
